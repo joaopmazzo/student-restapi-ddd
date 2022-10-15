00f4888761f300e88c4fec8d191cd707118001b9
João Paulo Mazzo <mazzojp@gmail.com>
2022-10-15 18:30:34 -0300

    Initial commit

---
+++ src/main/java/com/joaopmazzo/studentrestapiddd/StudentRestapiDddApplication.java
@@ -0,0 +1,13 @@
+package com.joaopmazzo.studentrestapiddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRestapiDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRestapiDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaopmazzo/studentrestapiddd/StudentRestapiDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopmazzo.studentrestapiddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRestapiDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
