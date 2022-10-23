61e00918a5ee513b073a5ddc261e44b199064750
João Paulo Mazzo <mazzojp@gmail.com>
2022-10-23 12:19:08 -0300

    Update StudentRestapiDddApplication.java

--- src/main/java/com/joaopmazzo/studentrestapiddd/StudentRestapiDddApplication.java
+++ src/main/java/com/joaopmazzo/studentrestapiddd/StudentRestapiDddApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentRestapiDddApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentRestapiDddApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRestapiDddApplication.class, args);
+    }
 
 }
