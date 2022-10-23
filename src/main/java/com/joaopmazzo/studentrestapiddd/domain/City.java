e856d7b266b80c0c3f5fb76477e11c488e1c9c1f
João Paulo Mazzo <mazzojp@gmail.com>
2022-10-23 12:06:46 -0300

    Create City.java domain class

---
+++ src/main/java/com/joaopmazzo/studentrestapiddd/domain/City.java
@@ -0,0 +1,23 @@
+package com.joaopmazzo.studentrestapiddd.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 4, max = 100)
+    private String city;
+
+}
