16063f12e06bddaab4f3a0a3f415ab92b869c92c
João Paulo Mazzo <mazzojp@gmail.com>
2022-10-23 12:19:24 -0300

    Update StudentRestapiDddApplicationTests.java

--- src/test/java/com/joaopmazzo/studentrestapiddd/StudentRestapiDddApplicationTests.java
+++ src/test/java/com/joaopmazzo/studentrestapiddd/StudentRestapiDddApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StudentRestapiDddApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
