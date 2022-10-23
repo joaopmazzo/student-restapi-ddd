57367b285f17624b7e67ee3f6207bd28d02e6d04
João Paulo Mazzo <mazzojp@gmail.com>
2022-10-23 12:03:13 -0300

    Create import.sql to initialize database with some data.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO COUNTRY_MODEL VALUES (1, 'Brazil'), (2, 'United States'), (3, 'Netherlands'), (4, 'Japan'), (5, 'Tokyo');
+
+INSERT INTO CITY_MODEL VALUES (1, 'Brasilia'), (2, 'Washington'), (3, 'Amsterdam'), (4, 'Tokyo'), (5, 'Rome');
+
+INSERT INTO STATE_MODEL VALUES (1, 'São Paulo'), (2, 'Rio de Janeiro'), (3, 'North Holland'), (4, 'Zeeland'), (5, 'Illinois'), (6, 'Province of Como'), (7, 'Province of Salerno'), (8, 'Kanto'), (9, 'Tohoku');
